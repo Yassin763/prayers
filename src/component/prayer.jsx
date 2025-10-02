189125f136834f6331240d61835f1ec2475a0a89
Yassin763 <yoyo_yasin@yahoo.com>
2025-10-02 04:51:28 +0300

    Add files via upload

---
+++ src/component/prayer.jsx
@@ -0,0 +1,12 @@
+function Prayer({name , time}) {
+    return ( 
+        <>
+        <div className="prayer">
+            <p className="prayer_name">{name}</p>
+            <p className="prayer_time">{time}</p>
+        </div>
+        </>
+     );
+}
+
+export default Prayer;
